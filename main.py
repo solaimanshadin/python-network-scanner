db534c1948be642c596ef37e8275eacd603ecf65
Solaiman Shadin <solaimanshadin@gmail.com>
2023-02-02 22:18:59 +0600

    [mod] formatted result

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def scan(ip):
      request = scapy.ARP(pdst=ip)
      bordcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
      bordcast_arp = bordcast/request
-     answered, unanswared=scapy.srp(bordcast_arp, timeout=1)
-     print(answered.summary())
+     answered=scapy.srp(bordcast_arp, timeout=1, verbose=False)[0]
+     print("Mac \t\t\tIP \n------------------------------------------")
+     for packet in answered:
+          print(packet[1].hwsrc + "\t",packet[1].psrc )
 
 scan("192.168.0.100/24")
